006da73c4f7a11910e80f949478c4b7ad47b4217
AshutoshSingh23 <63116827+AshutoshSingh23@users.noreply.github.com>
2020-04-13 17:46:03 +0530

    Created multiplication
    it prints the multiplication of two numbers.

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n1,n2,m;
+    cout << "Enter the numbers:" << endl;
+    cin >> n1 >> n2;
+    m=n1*n2;
+    cout << "The multiplication of the numbers is:" << m;
+    return 0;
+}
